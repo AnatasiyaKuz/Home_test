fb473e016c33f4cf6c24a6562a9a7c2ed73860f0
Anastasiya <nastya.kuzich.02@mail.ru>
2023-10-04 23:30:08 +0300

    added test tast

--- src/App.js
+++ src/App.js
@@ -1,24 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import { categories } from "./data";
+import { useEffect, useState } from "react";
+import KeyboardArrowUpOutlinedIcon from "@mui/icons-material/KeyboardArrowUpOutlined";
+import KeyboardArrowDownOutlinedIcon from "@mui/icons-material/KeyboardArrowDownOutlined";
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+  // const [width, setWidth] = useState(window.innerWidth);
+  // const [sliceCategories, setSliceCategories] = useState(6);
+  // const [categoriesToShow, setCategoriesToShow] = useState([]);
+
+  // useEffect(() => {
+  //   const handleResize = event => setWidth(event.target.innerWidth);
+  //   window.addEventListener("resize", handleResize);
+  //   return () => {
+  //     window.removeEventListener("resize", handleResize);
+  //   };
+  // }, []);
+
+  // useEffect(()=>{
+  //   if(width<961 && width>601 && !isOpen){
+  //     setSliceCategories(4);
+  //     setCategoriesToShow(categories.slice(0, sliceCategories))
+  //   } else if (!isOpen) {
+  //     setSliceCategories(6);
+  //     setCategoriesToShow(categories.slice(0, sliceCategories))
+  //   } else {
+  //     setCategoriesToShow(categories)
+  //   }
+  // }, [width, isOpen])
+
+  const handleOnClick = () => setIsOpen(!isOpen);
+
+  const categoriesToShow = isOpen ? categories : categories.slice(0, 6);
+
+  useEffect(() => {
+    // Если раздел открыт, добавляем класс "open_section" для плавного появления элементов
+    if (isOpen) {
+      const links = document.querySelectorAll(".link");
+      links.forEach((link) => {
+        link.classList.add("open_section");
+      });
+    }
+  }, [isOpen]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <section className="home_section">
+      <div className="content">
+        <h2>Join 2 million+ Australians finding better</h2>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Finder's team of 40+ experts will help you find the right choices in
+          over 100 categories.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+        <div className='categories_section'>
+          {categoriesToShow.map((category, index) => {
+            return (
+              <a key={index} href={category.link} className={`link ${isOpen?'open_section':'close_section'}`}>
+                {category.icon}
+                {category.name}
+              </a>
+            );
+          })}
+        </div>
+        <div className="open_button">
+          <button onClick={handleOnClick}>
+            <div>Show {isOpen ? "less" : "more"} categories</div>
+            {isOpen ? (
+              <KeyboardArrowUpOutlinedIcon className="arrowIcon" />
+            ) : (
+              <KeyboardArrowDownOutlinedIcon className="arrowIcon" />
+            )}
+          </button>
+        </div>
+      </div>
+    </section>
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,145 @@
+import CreditCardOutlinedIcon from '@mui/icons-material/CreditCardOutlined';
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import MonitorHeartOutlinedIcon from '@mui/icons-material/MonitorHeartOutlined';
+import DirectionsCarOutlinedIcon from '@mui/icons-material/DirectionsCarOutlined';
+import AccountBalanceOutlinedIcon from '@mui/icons-material/AccountBalanceOutlined';
+import ElectricBoltOutlinedIcon from '@mui/icons-material/ElectricBoltOutlined';
+import LocalAtmOutlinedIcon from '@mui/icons-material/LocalAtmOutlined';
+import AdUnitsOutlinedIcon from '@mui/icons-material/AdUnitsOutlined';
+import StarBorderPurple500OutlinedIcon from '@mui/icons-material/StarBorderPurple500Outlined';
+import TrendingUpOutlinedIcon from '@mui/icons-material/TrendingUpOutlined';
+import BeachAccessOutlinedIcon from '@mui/icons-material/BeachAccessOutlined';
+import AirplanemodeActiveOutlinedIcon from '@mui/icons-material/AirplanemodeActiveOutlined';
+import CalendarMonthOutlinedIcon from '@mui/icons-material/CalendarMonthOutlined';
+import CurrencyBitcoinOutlinedIcon from '@mui/icons-material/CurrencyBitcoinOutlined';
+import PetsOutlinedIcon from '@mui/icons-material/PetsOutlined';
+import AttachMoneyOutlinedIcon from '@mui/icons-material/AttachMoneyOutlined';
+import PublicOutlinedIcon from '@mui/icons-material/PublicOutlined';
+import ShoppingBagOutlinedIcon from '@mui/icons-material/ShoppingBagOutlined';
+import SubscriptionsOutlinedIcon from '@mui/icons-material/SubscriptionsOutlined';
+import HourglassTopOutlinedIcon from '@mui/icons-material/HourglassTopOutlined';
+
+const categories = [
+    {
+        name: 'Credit Cards',
+        icon: <CreditCardOutlinedIcon className="icon"/>,
+        link: 'https://www.finder.com.au/credit-cards'
+    },
+    {
+        name: 'Home Loans',
+        icon: <HomeOutlinedIcon className='icon'/>,
+        link: 'https://www.finder.com.au/home-loans'
+    },
+    {
+        name: 'Health Insurance',
+        icon: <MonitorHeartOutlinedIcon className='icon'/>,
+        link: 'https://www.finder.com.au/health-insurance'
+    },
+    {
+        name: 'Car Insurance',
+        icon: <DirectionsCarOutlinedIcon className='icon'/>,
+        link: 'https://www.finder.com.au/car-insurance'
+    },
+    {
+        name: 'High-Interest Saving',
+        icon: <AccountBalanceOutlinedIcon className='icon'/>,
+        link: 'https://www.finder.com.au/savings-accounts/high-interest-savings-accounts'
+    },
+    {
+        name: 'Electricity & Gas',
+        icon: <ElectricBoltOutlinedIcon className='icon'/>,
+        link: 'https://www.finder.com.au/energy'
+    },
+    {
+        name: 'Personal Loans',
+        icon: <LocalAtmOutlinedIcon className='icon'/>,
+        link: 'https://www.finder.com.au/personal-loans'
+    },
+    {
+        name: 'Mobile Plans',
+        icon: <AdUnitsOutlinedIcon className='icon'/>,
+        link: 'https://www.finder.com.au/mobile-plans'
+    },
+    {
+        name: 'Broadband & NBN',
+        icon: <HomeOutlinedIcon className='icon'/>,
+        link: 'https://www.finder.com.au/broadband-plans'
+    },
+    {
+        name: 'Super Funds',
+        icon: <StarBorderPurple500OutlinedIcon className='icon'/>,
+        link: 'https://www.finder.com.au/super-funds'
+    },
+    {
+        name: 'Share Trading',
+        icon: <TrendingUpOutlinedIcon className="icon"/>,
+        link: 'https://www.finder.com.au/share-trading'
+    },
+    {
+        name: 'Life Insurance',
+        icon: <BeachAccessOutlinedIcon className='icon'/>,
+        link: 'https://www.finder.com.au/life-insurance'
+    },
+    {
+        name: 'Home Insurance',
+        icon: <HomeOutlinedIcon className='icon'/>,
+        link: 'https://www.finder.com.au/home-insurance'
+    },
+    {
+        name: 'Travel Insurance',
+        icon: <AirplanemodeActiveOutlinedIcon className='icon'/>,
+        link: 'https://www.finder.com.au/travel-insurance'
+    },
+    {
+        name: 'Travel Deals',
+        icon: <CalendarMonthOutlinedIcon className='icon'/>,
+        link: 'https://www.finder.com.au/travel'
+    },
+    {
+        name: 'Crypto',
+        icon: <CurrencyBitcoinOutlinedIcon className='icon'/>,
+        link: 'https://www.finder.com.au/cryptocurrency'
+    },
+    {
+        name: 'Car Loans',
+        icon: <DirectionsCarOutlinedIcon className='icon'/>,
+        link: 'https://www.finder.com.au/car-loans'
+    },
+    {
+        name: 'Pet Insurance',
+        icon: <PetsOutlinedIcon className='icon'/>,
+        link: 'https://www.finder.com.au/pet-insurance'
+    },
+    {
+        name: 'Transaction Accounts',
+        icon: <AttachMoneyOutlinedIcon className='icon'/>,
+        link: 'https://www.finder.com.au/bank-accounts'
+    },
+    {
+        name: 'Money Transfers',
+        icon: <PublicOutlinedIcon className='icon'/>,
+        link: 'https://www.finder.com.au/international-money-transfers'
+    },
+    {
+        name: 'Shopping Deals',
+        icon: <ShoppingBagOutlinedIcon className='icon'/>,
+        link: 'https://www.finder.com.au/deals'
+    },
+    {
+        name: 'Streaming',
+        icon: <SubscriptionsOutlinedIcon className='icon'/>,
+        link: 'https://www.finder.com.au/internet-tv'
+    },
+    {
+        name: 'Business Insurance',
+        icon: <LocalAtmOutlinedIcon className='icon'/>,
+        link: 'https://www.finder.com.au/business-insurance'
+    },
+    {
+        name: 'Short Term Loans',
+        icon: <HourglassTopOutlinedIcon className='icon'/>,
+        link: 'https://www.finder.com.au/payday-loans'
+    },
+];
+
+export {categories};
